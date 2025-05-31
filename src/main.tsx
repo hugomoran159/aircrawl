09fc670f1eeff7d48ff271c5a394b5562d2f7899
hugomoran159 <78805535+hugomoran159@users.noreply.github.com>
2025-05-31 16:40:23 +0100

    posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
 import { createRoot } from 'react-dom/client'
+import { PostHogProvider } from 'posthog-js/react'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <PostHogProvider
+    apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+    options={{
+      api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+      capture_exceptions: true,
+      debug: import.meta.env.MODE === 'development',
+    }}
+  >
+    <App />
+  </PostHogProvider>
+);
